2b86fbfe22ab28a1107287dad7e97cdb4bdb9e12
KLtechworks <kelly.w3bd@gmail.com>
2026-01-05 14:22:16 +1100

    fix backend exercises folder upload

---
+++ part5/part5-backend-exercises-5.1-5.23/controllers/blogs.js
@@ -0,0 +1,75 @@
+const Blog = require('../models/blog')
+const blogsRouter = require('express').Router()
+const { userExtractor } = require('../utils/middleware')
+
+blogsRouter.get('/', (request, response) => {
+  Blog.find({})
+    .populate('user', { username: 1, name: 1, id: 1 })
+    .then(blogs => {
+      response.json(blogs)
+    })
+})
+
+blogsRouter.post('/', userExtractor, async (request, response) => {
+  const user = request.user
+  const blog = new Blog(request.body)
+
+  blog.likes = blog.likes | 0
+  blog.user = user._id
+
+  if (!blog.title || !blog.url) {
+    return response.status(400).send({ error: 'title or url missing' })
+  }
+
+  user.blogs = user.blogs.concat(blog._id)
+  await user.save()
+
+  const savedBlog = await blog.save()
+
+  response.status(201).json(savedBlog)
+})
+
+blogsRouter.delete('/:id', userExtractor, async (request, response) => {
+  const user = request.user
+  const blog = await Blog.findById(request.params.id)
+
+  if (!blog) {
+    return response.status(204).end()
+  }
+
+  if (user.id.toString() !== blog.user.toString()) {
+    return response.status(403).json({ error: 'user not authorized' })
+  }
+
+  user.blogs = user.blogs.filter(b => b.id.toString() !== blog.id.toString())
+
+  await blog.deleteOne()
+  response.status(204).end()
+})
+
+// 5.8: Blog List Frontend, step 8
+blogsRouter.put('/:id', async (request, response) => {
+  const body = request.body
+
+  const blogToUpdate = {
+    title: body.title,
+    author: body.author,
+    url: body.url,
+    likes: body.likes || 0,  
+    user: body.user?._id || body.user  
+  }
+  
+  const updatedBlog = await Blog.findByIdAndUpdate(
+    request.params.id,
+    blogToUpdate,
+    { new: true, runValidators: true, context: 'query' }
+  ).populate('user', { username: 1, name: 1, id: 1 })  
+
+  if (!updatedBlog) {
+    return response.status(404).json({ error: 'blog not found' })
+  }
+
+  response.json(updatedBlog)
+})
+
+module.exports = blogsRouter
